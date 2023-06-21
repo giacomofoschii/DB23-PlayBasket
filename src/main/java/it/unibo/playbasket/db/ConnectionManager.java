f4e748ca52038593f263bd7ab93b90cfdcdf6a26
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2023-06-22 00:08:42 +0200

    Added db-oriented classes

---
+++ src/main/java/it/unibo/playbasket/db/ConnectionManager.java
@@ -0,0 +1,43 @@
+package it.unibo.playbasket.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Class that manages the connection to the database.
+ */
+public class ConnectionManager {
+
+    private final String user, password, dbname;
+
+    /**
+     * Constructor.
+     */
+    public ConnectionManager() {
+        this.user = "root";
+        this.password = "VisualStudioCode13";
+        this.dbname = "db23_playbasket";
+    }
+
+    /**
+     * Creates a connection to the database.
+     * 
+     * @return the connection to the database.
+     * @throws SQLException if the connection cannot be established.
+     * @throws ClassNotFoundException if the driver cannot be found.
+     */
+    public Connection getSQLConnection() {
+
+        final String dbLink = "jdbc:mysql://localhost:3306/" + this.dbname;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(
+                    dbLink, this.user, this.password);
+        } catch (final SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        } catch (final ClassNotFoundException e) {
+            throw new IllegalStateException("Cannot find the driver in the classpath!", e);
+        }
+    }
+}
