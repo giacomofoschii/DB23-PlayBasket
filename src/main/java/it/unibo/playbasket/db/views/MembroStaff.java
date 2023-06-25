f84cea77ffb1a8c82f47d3170d1d9f8f5264eee1
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2023-06-25 23:54:20 +0200

    Implemented Societa management

--- src/main/java/it/unibo/playbasket/db/views/MembroStaff.java
+++ src/main/java/it/unibo/playbasket/db/views/MembroStaff.java
@@ -5,13 +5,15 @@ public class MembroStaff {
     private String nome;
     private String cognome;
     private int eta;
+    private int stipendio;
     private String ruolo;
     private String specializzazione;
     private int annoPatentino;
 
-    public MembroStaff(String nome, String cognome, int eta, String ruolo, String specializzazione, int annoPatentino) {
+    public MembroStaff(String nome, String cognome, int eta, int stipendio, String ruolo, String specializzazione, int annoPatentino) {
         this.nome = nome;
         this.cognome = cognome;
+        this.stipendio = stipendio;
         this.eta = eta;
         this.ruolo = ruolo;
         this.specializzazione = specializzazione;
@@ -28,6 +30,10 @@ public class MembroStaff {
 
     public int getEta() {
         return eta;
+    }
+
+    public int getStipendio() {
+        return stipendio;
     }
 
     public String getRuolo() {
